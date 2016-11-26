a50fba8f099decbaf91dfddd321bbf502ad6c45a
杜兴华 <xinghua.0108@hotmail.com>
2016-11-26 16:25:23 +0800

    more language

---
+++ js/dynamicLang.js
@@ -0,0 +1,44 @@
+/**
+ * Created by test on 16/11/25.
+ */
+Lang.prototype.pack.zh = {
+    "token":{
+        "bitpie - Best Bitcoin Wallet!":"比特派-最好用的比特币钱包！",
+        "About":"关于",
+        "Features":"特色",
+        "Contact":"联系我们",
+        "bitpie":"比特派",
+        "Wallet & P2P Exchange":"钱包 和 P2P 交易",
+        "Android":"安卓",
+        "Download":"下载",
+        "Google Play":"谷歌商店",
+        "iOS":"苹果",
+        "App Store":"苹果商店",
+        "Bitpie is developed by":"比特派是由",
+        "Bither":"比太",
+        "team. With bitpie, you can have a secured bitcoin wallet as simple as possible, and you can also buy/sell bitcoins in an easy way.":
+            "团队研发的比特币钱包，它能让您轻松安全的使用比特币，并且还能方便的买卖比特币。",
+        "Bitpie will also support other blockchain assets in the future, so it will be the wallet for everything, and you should try it now!":
+            "比特派还将支持多种区块链资产，未来您可以使用一个钱包来管理一切，赶紧试试吧！",
+        "Wallets":"钱包",
+        "Simpliest Bitcoin Wallet Ever!":"简单易用的比特币钱包！",
+        "As a bitcoin wallet, bitpie means you control your private keys, and nobody can touch your assets. With our special design, you do not need to sync the blockchain data with the bitcon network, and sending/receiving bitcoins are processed so quickly. Also bitpie supports dynamic transaction fee and transaction accelerator, that means your transaction can easily go through even when network congestion.":
+            "作为钱包，比特派意味着您自己掌握私钥，其他人无法触碰您的财产。通过我们的优化，您可以快捷的收发比特币，并且比特派还支持动态矿工费及交易加速器，这意味着即便是遇到了网络大拥堵，您的交易也能迅速得到确认。",
+        "P2P Exchange:Easiest Way to Buy/Sell Bitcoins with Other Peers!":"P2P 交易：轻松的与其他人买卖比特币！",
+        "Bitpie has built-in escrow function based on Multisig technology, so you can easily buy/sell bitcoins with the dealers all over the world in a secured environment.":"比特派内置了给予多重签名技术的托管功能，因此您可以安全的与世界各地的交易员买卖比特币。",
+        "Security":"安全：",
+        "Bither team has so many reputations in the past, and Bither wallet has been recommended by Bitcoin.org for a long time. We know how to make secured bitcoin wallet, and how to protect your blockchain assets. The bitpie wallet is totally based on Bither technology, so it is robust and trustable.":
+            "比太团队在安全方面一直享有良好的声誉，比太钱包也是中国唯一一个被 Bitcoin.org 官方推荐的比特币钱包，我们懂得如何开发安全的比特币钱包以及如何保护您的区块链资产，比特派钱包完全基于比太技术，值得信赖。",
+        "Keep in Touch:":"保持联络",
+        "P2P Exchange":"P2P 交易：",
+        "Wallets":"钱包：",
+        "Easiest Way to Buy/Sell Bitcoins with Other Peers!":"轻松的与其他人买卖比特币！",
+        "Email":"邮箱",
+        "QQ group:493077876":"QQ群：493077876",
+        "Home":"首页",
+        "The bitpie wallet is based on":"比特派钱包基于",
+        "Bither":"比太",
+        "technology.":"技术。"
+
+    }
+}
