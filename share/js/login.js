38c6c57383a1f35469e7912a7a0adb9f5b06fbb5
zhtzzs <zhtzzs@163.com>
2018-11-12 20:02:13 +0800

    lastshare

--- share/js/login.js
+++ share/js/login.js
@@ -94,13 +94,11 @@ $(function(){
     dsBridge.call("bp_interaction.getShareLink","",function (v) {
       console.log(v)
       console.log('0')
+      $("#share_qr").html(' ')
       new QRCode(document.getElementById('share_qr'), v);
       html2canvas(document.querySelector("#share-canvas"),opts).then(function(canvas) {
         var img = canvas.toDataURL('image/png')
-        console.log('1')
         dsBridge.call("bp_interaction.openShareBase64Img",img, function (v) {
-          console.log(v)
-          console.log('2')
         })
       });
 
