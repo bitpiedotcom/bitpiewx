21c428fb2537b07249af7f144b0fef1e47c7bcb5
zhtzzs <zhtzzs@163.com>
2018-11-19 19:21:47 +0800

    online

--- share/i18n/en.js
+++ share/i18n/en.js
@@ -4,8 +4,8 @@ const en = {
     phoneNum: 'Enter phone number',
     intro3: 'Enter code',
     intro4: 'Enter correct phone number',
-    title1: 'Select country',
-    title2: 'Phone number',
+    title1: 'Country',
+    title2: 'Phone NO',
     title3: 'SMS code',
     getCode: 'Get code',
     getCode2: 'Sent',
@@ -19,7 +19,7 @@ const en = {
     intro: {
       1: '1. Activity time: From November 15, 2018 12 p.m. to November 30, 2018 11.59 p.m. （HongKong Time）',
       2: '2. How to get the rewards: After inviting a valid user, you can view the reward details in “Bitpie– Reward Centre”;',
-      3: '3. Rewards details：The invitee can get a free EOS account Coupon',
+      3: '3. Rewards details：The invitee can get a free EOS account Coupon<br/>',
       4: '* The phone number is only the proof for receiving the reward, real-name authentication is not compulsory while using Bitpie; ',
       5: '* Bitpie have the right to cancel rewards that are obtained through improper ways;',
       6: '* The same phone number, the same UID, the same terminal number or characteristics that are reasonably displayed as the same user are treated as the same user;'
@@ -35,19 +35,22 @@ const en = {
   },
   share:{
     banner:'images/share_en.png',
+    btn1:'Copy Link',
+    btn2:'Share image',
     intro: [
       'Activity rules:',
       '1. Activity time: From November 15, 2018 12 p.m. to November 30, 2018 11.59 p.m. （HongKong Time）',
       '2. Rewards details ',
       '【1】Channel Rewards: By inviting valid users, the inviter can get all EOS DApp rewards on Bitpie from the invitees. No ceiling for Invitation times, no ceiling for Rewards; ',
       '【2】Resource Rewards: By inviting valid users, both the inviter and the invitee can get a free EOS account Coupon or an EOS-Resource-Help Coupon;',
-      '3. How to get the rewards: After inviting a valid user, you can view the reward details in “Bitpie– Reward Centre”;',
+      '3. How to get the rewards: After inviting a valid user, you can view the reward details in “Bitpie– Reward Centre”;<br/>',
       '* The phone number is only the proof for receiving the reward, real-name authentication is not compulsory while using Bitpie;',
       '* Bitpie have the right to cancel rewards that are obtained through improper ways;',
       '* The same phone number, the same UID, the same terminal number or characteristics that are reasonably displayed as the same user are treated as the same user;',
       '* To the extent permitted by law, Bitpie reserves the right of final interpretation of this activity.'
 
 
-    ]
+    ],
+    font: 'I am using Bitpie Wallet, it invites you to play the most popular DApps, click to get a free EOS account. The amount is limited, which is subject to availability.'
   }
 }
